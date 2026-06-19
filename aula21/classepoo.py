4b6ae2d8688b4606bcbbfa3933bc35a963001779
rafael <141762633+RafaelDevGomes@users.noreply.github.com>
2026-06-19 11:21:35 -0300

    a

---
+++ aula21/classepoo.py
@@ -0,0 +1,23 @@
+# precisamos criar um molde de uma pessoa. => class
+# caracteristicas -> atributos e são representadas via variáveis
+# ações -> métodos são representadas via de funções
+
+class Pessoa: 
+    # constructor
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf # cpf atributo publico 
+        # self._cpf = cpf atributo privado 
+    # método de apresentação
+    def apresentar(self) -> str:
+        return f"Olá, meu nome é {self.nome}"
+    
+pessoa0 = Pessoa("Ana Lima", "123")
+pessoa1 = Pessoa("Rafael Sigma", "123")
+
+print(pessoa0.apresentar())
+print(pessoa1.apresentar())
+
+
+
+
