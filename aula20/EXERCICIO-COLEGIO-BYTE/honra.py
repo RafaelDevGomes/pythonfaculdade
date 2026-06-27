6e28e1dfb9e0bf005579c18d559d1a46325a1dd4
RafaelDevGomes <gomesdadilvarafael01@gmail.com>
2026-06-26 23:03:00 -0300

    a

--- aula20/EXERCICIO-COLEGIO-BYTE/honra.py
+++
@@ -1,8 +0,0 @@
-print("---- " \
-    " 🏫📗Olá, seja bem vindo ao sistema do Colégio Byte.🏫📗 " \
-    " ----")
-
-def verificar_situacao(media: float, verificar_honra: bool):
-    if media > 9 and verificar_honra:
-    print("a")
-    return True
--- fixaxoes/ex9def.py
+++ fixaxoes/ex9def.py
@@ -1,7 +1,7 @@
 def calcular_media(notas):
     sum(notas)
     return sum(notas) / len(notas) # len é a quantidade de coisas colocadas na variavel (evitar ficar digitando toda hora a quantidade, apenas digitam o len e ta safe)
-# ent geralmente usam isso pra fazer media ne sem precisar ficar declarando a quantidade de notas, eu precisaria apenas digitar as notas e usar o len na propria variavel notas?
+# ent geralmente usam isso pra fazer media ne sem precisar ficar declarando a quantidade de notas, eu precisaria apenas digitar as notas e usar o len na propria variavel notas
 media = calcular_media([10, 5, 3, 2, 6])
 print(media)
 
